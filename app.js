8ea7ea443b3b7619e2f48bc8a27d9ce28cab68cd
Supphawit <s.wuttiprasit@gmail.com>
2018-09-25 21:08:26 +0700

    init

--- app.js
+++ app.js
@@ -10,134 +10,36 @@ app.get('/', function (req, res) {
 app.use('/client', express.static(__dirname + '/client'))
 
 serv.listen(2000)
-console.log('Server connection')
 
-var SOCKET_LIST = {}
-
-var Entity = function () {
-  var self = {
-    x: 250,
-    y: 250,
-    spdX: 0,
-    spdY: 0,
-    id: ""
-  }
-
-  self.update = function () {
-    self.updatePosition()
-  }
-  self.updatePosition = function () {
-    self.x += self.spdX
-    self.y += self.spdY
-  }
-  return self
-}
-
-var Player = function (id) {
-  var self = Entity()
-  self.id = id
-  self.number = "" + Math.floor(10 * Math.random())
-  self.pressingRight = false
-  self.pressingLeft = false
-  self.pressingUp = false
-  self.pressingDown = false
-  self.maxSpd = 10
-
-  var super_update = self.update
-  self.update = function () {
-    self.updateSpd()
-    super_update()
-  }
-
-  self.updateSpd = function () {
-    if (self.pressingRight)
-      self.spdX = self.maxSpd
-    else if (self.pressingLeft)
-      self.spdX = -self.maxSpd
-    else
-      self.spdX = 0
-
-    if (self.pressingUp)
-      self.spdY = -self.maxSpd
-    else if (self.pressingDown)
-      self.spdY = self.maxSpd
-    else
-      self.spdY = 0
-  }
-  Player.list[id] = self
-
-  // self.updatePosition = function () {
-  //   if (self.pressingRight)
-  //     self.x += self.maxSpd
-
-  //   if (self.pressingLeft)
-  //     self.x -= self.maxSpd
-
-  //   if (self.pressingUp)
-  //     self.y -= self.maxSpd
-
-  //   if (self.pressingDown)
-  //     self.y += self.maxSpd
-
-  // }
-  return self
-}
-
-Player.list = {}
-Player.onConnect = function (socket) {
-  var player = Player(socket.id)
-  socket.on('keyPress', function (data) {
-    if (data.inputId === 'left')
-      player.pressingLeft = data.state
-    else if (data.inputId === 'right')
-      player.pressingRight = data.state
-    else if (data.inputId === 'up')
-      player.pressingUp = data.state
-    else if (data.inputId === 'down')
-      player.pressingDown = data.state
+app.get('/write', (req, res) => {
+  var fs = require('fs')
+  fs.writeFile("./script/" + req.query.namefile + ".py", req.query.python, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    res.redirect("/run?file=" + req.query.namefile );
   })
+});
 
-}
+app.get('/run',(req, res) => {
+  const { spawn } = require('child_process');
 
-Player.onDisconnect = function (socket) {
-  delete Player.list[socket.id]
-}
+  const  process = spawn('python',["./script/" + req.query.file + ".py"] );
 
-Player.update = function(){
-  var pack = []
-  for (var i in Player.list) {
-    var player = Player.list[i]
-    player.update()
-    pack.push({
-      x: player.x,
-      y: player.y,
-      number: player.number
-    })
-  }
+  process.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+  res.send(data.toString())
+  });
+  
+  process.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+    res.send(data.toString())
+  });
 
-}
-var io = require('socket.io')(serv, {})
+  process.on('close', (code) => {
+    if (code != 0){
+      console.log(`child process exited with code ${code}`);
 
-io.sockets.on('connection', function (socket) {
-  socket.id = Math.random()
-  SOCKET_LIST[socket.id] = socket
-
-  Player.onConnect(socket)
-
-  socket.on('disconnect', function () {
-    delete SOCKET_LIST[socket.id]
-    Player.onDisconnect(socket)
-  })
-
-})
-
-setInterval(function () {
-  var pack = Player.update()
-
-  for (var i in SOCKET_LIST) {
-    var socket = SOCKET_LIST[i]
-    socket.emit('newPosition', pack)
-  }
-
-
-}, 1000 / 25)
+    }
+  });
+});
---
+++ script/printname.py
@@ -0,0 +1,1 @@
+asfsadfjsdafs
