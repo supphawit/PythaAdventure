ed81bd51ca9ac03d240040080253ad4dc86f0c0b
supphawit <36230326+supphawit@users.noreply.github.com>
2019-05-19 20:07:02 +0000

    edit speed char

--- script/online_compiler.py
+++ script/online_compiler.py
@@ -1,10 +1,3 @@
-def fibo_re(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibo_re(n-1) + fibo_re(n-2)
 
-# ผลลัพธ์ของ Fibonacci ที่ n = 10ค่า factorial ของเลขจำนวน
-print (fibo_re(10))
+for i in range(0,10):
+    print ("i = ", i)
